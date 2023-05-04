5d0b1627bcd10e6a7bebac6f9e333c6296cb83d5
steffen.rhinow@hubject.com <steffen.rhinow@hubject.com>
2023-05-04 23:18:18 +0200

    Added optional parameter to filter in RCP for the root type

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NzUzMw/spectral.js?branch_slug=main&token=0729c3ee-7831-4751-8fa5-76d4cd44e703";
+export default { extends: ruleset };
